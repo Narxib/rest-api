5d5adde2ccefaa2990ab63e844e47ebb5bf0d0ce
Brian <brianzaragoza25@gmail.com>
2023-08-24 11:50:20 +0200

    updated app.js

--- app.js
+++ app.js
@@ -6,6 +6,27 @@ const app = express()
 
 app.use(express.json())
 
+app.use(cors({
+    origin: (origin, callback) => {
+        const ACCEPTED_ORIGINS = [
+            'http://localhost:8080',
+            'http://localhost:1234',
+            'https://movies.com',
+            'https://midu.dev'
+        ]
+
+        if (ACCEPTED_ORIGINS.includes(origin)) {
+            return callback(null, true)
+        }
+
+        if (!origin) {
+            return callback(null, true)
+        }
+
+        return callback(new Error('Not allowed by CORS'))
+    }
+}))
+
 app.get("/", (req, res) =>
     res.json({ mess: "Hola munfo" })
 )
