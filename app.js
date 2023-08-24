9f34ce4d89ba9c93a5c3ff13eb14e29c6746ec6d
Brian <brianzaragoza25@gmail.com>
2023-08-24 11:37:32 +0200

    primer commit

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express")
+const crypto = require("node:crypto")
+const movies = require("./movies.json")
+const { validateMovie, validatePartialMovie } = require("./movieSchema")
+const app = express()
+
+app.use(express.json())
+
+app.get("/", (req, res) =>
+    res.json({ mess: "Hola munfo" })
+)
+
+
+app.get("/movies", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    const { genre } = req.query
+    if (genre) {
+        const filteredMovies = movies.filter(movie =>
+            movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+        )
+        return res.json(filteredMovies)
+    }
+    res.json(movies)
+})
+
+app.post('/movies', (req, res) => {
+    const result = validateMovie(req.body)
+
+    if (!result.success) {
+        // 422 Unprocessable Entity
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    // en base de datos
+    const newMovie = {
+        id: crypto.randomUUID(), // uuid v4
+        ...result.data
+    }
+
+    // Esto no sería REST, porque estamos guardando
+    // el estado de la aplicación en memoria
+    movies.push(newMovie)
+
+    res.status(201).json(newMovie)
+})
+
+
+
+app.get("/movies/:id", (req, res) => {
+    const { id } = req.params
+    const movie = movies.find(movie => movie.id === id)
+    if (movie) return res.json(movie)
+
+    res.status(404).json({ error: "Movie not found" })
+
+})
+
+app.patch("/movies/:id", (req, res) => {
+
+    const result = validatePartialMovie(req.body)
+
+    if (!result.success) {
+        return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+    const { id } = req.params
+
+    const movieIndex = movies.findIndex(movie => movie.id == id)
+    if (movieIndex < 0) return res.status(404).json({ message: "Movie not found" })
+
+    const updateMovie = {
+        ...movies[movieIndex],
+        ...result.data
+    }
+    movies[movieIndex] = updateMovie
+    return res.json(updateMovie)
+})
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, (req, res) => console.log(`Server on http://localhost:1234`))
---
+++ movieSchema.js
@@ -0,0 +1,46 @@
+const z = require("zod")
+
+const movieSchema = z.object({
+    title: z.string({
+        invalid_type_error: "Movie title must be a string",
+        required_error: "Title is required"
+    }),
+    year: z.number({
+        invalid_type_error: "Year must be a number",
+        required_error: "Year is required"
+    }).int().min(1900).max(2025),
+    director: z.string({
+        invalid_type_error: "Director must be a string",
+        required_error: "Director is required"
+    }),
+    duration: z.number({
+        invalid_type_error: "Duration must be a number",
+        required_error: "Duration is required"
+    }).int().positive().max(720),
+    poster: z.string({
+        invalid_type_error: "The poster must be a url with .jpg , .png or .svg extension",
+        required_error: "Poster is required"
+    }).url().endsWith(".webp"),
+    genre: z.array(
+        z.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
+        {
+            required_error: 'Movie genre is required.',
+            invalid_type_error: 'Movie genre must be an array of enum Genre'
+        }
+    ),
+    rate: z.number({
+        invalid_type_error: "Rate must be a number",
+        required_error: "Rate is required"
+    }).min(0).max(10)
+})
+
+
+function validateMovie(input) {
+    return movieSchema.safeParse(input)
+}
+
+function validatePartialMovie(input) {
+    return movieSchema.partial().safeParse(input)
+}
+
+module.exports = { validateMovie, validatePartialMovie }
