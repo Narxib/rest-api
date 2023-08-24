49d0987ae30a01d2cf6952a746d60e7ec08055fb
Brian <brianzaragoza25@gmail.com>
2023-08-24 11:54:34 +0200

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const crypto = require("node:crypto")
 const movies = require("./movies.json")
+const cors = require("cors")
 const { validateMovie, validatePartialMovie } = require("./movieSchema")
 const app = express()
 
